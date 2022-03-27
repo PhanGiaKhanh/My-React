bc8565e9e16aac6ed6f2998e8db0232b66a3f8e0
unknown <phangiakhanh90@gmail.com>
2022-03-27 15:42:12 +0700

    up next

--- Tutorial_Basic/udemy-fpt/01-starting-setup/src/App.js
+++ Tutorial_Basic/udemy-fpt/01-starting-setup/src/App.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Expense from "./components/Expenses/Expense";
-import FirstComponents from "./components/FirstComponents";
 import NewExpense from "./components/NewExpense/NewExpense";
+const DUMMY_EXPENSE = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-function App() {
-  const arr = ["Apple", "Banana", "Lemon"];
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
+  const addExpenseHandler = (expense) => {
+    // setExpenses([expense, ...expenses]);
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
   // return React.createElement(
   //   "div",
   //   {},
@@ -34,11 +37,10 @@ function App() {
   // );
   return (
     <>
-      <FirstComponents arr={arr} />
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expense items={expenses} />
     </>
   );
-}
+};
 
 export default App;
